1d95bf4c01406e8479959f2a55bf84c4897948f1
JoaoDias <joaodiasworking@gmail.com>
2024-09-08 21:32:10 -0300

    feat: Adding to run swagger on production just because this a proof of concept project

--- src/backend/CupcakeDias.API/Program.cs
+++ src/backend/CupcakeDias.API/Program.cs
@@ -77,14 +77,11 @@ builder.Services.AddScoped<IOrderService, OrderService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CupcakeDias API V1");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CupcakeDias API V1");
+});
 
 using (var scope = app.Services.CreateScope())
 {
